511ac0c61158730b06ff46a41626e3594e7a2556
BrayanL <brayan.loucher@outlook.com>
2021-04-15 22:32:25 +0200

    Mise en place du début de la fonction Addition. Push du nombre entré + reset de l'écran de la calculatrice.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 let btn = document.querySelectorAll(".number");
 function reset() {
     document.querySelector(".calc__number").innerHTML = "";
-}
+};
+let screen = document.querySelector("p");
+let operator = document.querySelectorAll(".blue");
+// let screen1 = screen.slice(0, -1); 
+let calcul = [];
 
 
 addEventListener("click", function(event) {
     let btn = event.target;
-    let screen = document.querySelector("p");
     screen.append(btn.textContent);
     if(btn.textContent === "AC"){
-        event.stopPropagation();
         reset()
     }
-
+    else if(btn.textContent === "+"){
+        calcul.push(screen.textContent.slice(0,-1));
+        console.log(calcul)
+        reset();
+    }
 });
