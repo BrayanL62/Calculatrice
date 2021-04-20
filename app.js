ad6881129507290f35bf8015e3a08cc052d39f2e
BrayanL <brayan.loucher@outlook.com>
2021-04-20 10:30:29 +0200

    correction du click entre les boutons.

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 let screen = document.querySelector(".calc__number");
+
 function reset() {
     screen.innerHTML = "";
 }
 
-addEventListener("click", function(event) {
+addEventListener("click", function (event) {
     let btn = event.target;
-    screen.append(btn.textContent);
-    console.log(screen.textContent);
-    
-    if(btn.textContent == "AC"){
-        console.log(screen.textContent.slice(0, -2));
-        reset();
+    if(btn.nodeName === "BUTTON") {
+        screen.append(btn.textContent);
+        if (btn.textContent == "AC") {
+            reset();
+        }
+        if (btn.textContent == "=") {
+            let calcul = [];
+            calcul.push(screen.textContent.slice(0, -1));
+            reset();
+            screen.append(eval(calcul[0]));
+            calcul.pop();
     }
-    if(btn.textContent == "=") {
-        let calcul = [];
-        console.log(screen.textContent.slice(0, -1));
-        calcul.push(screen.textContent.slice(0, -1));
-        console.log(calcul);
-        reset();
-        screen.append(eval(calcul[0]));
-        calcul.pop();
-    }
+}
     // if(btn.textContent == "+"){
     //     console.log(screen.textContent.slice(0, -1));
     //     calcul.push(Number(screen.textContent.slice(0, -1)));
     //     reset();
     //     console.log(calcul);
     // }
-    
+
 
 })
